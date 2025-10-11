967114477b1d257a66059e59ffdb8fd3a69cd0d7
benchinny <ben.chinny@gmail.com>
2025-10-11 09:38:09 -0400

    update fitting

--- ARCimageQualityModels/ARCtestWvInFocusMeanZstrehlLminusMeffectFitOnly.m
+++ ARCimageQualityModels/ARCtestWvInFocusMeanZstrehlLminusMeffectFitOnly.m
@@ -1,6 +1,6 @@
 %% LOADING DATA
 
-function ARCtestWvInFocusMeanZstrehlLminusMeffectFitOnly(subjNum)
+function ARCtestWvInFocusMeanZstrehlLminusMeffectFitOnly(subjNum,dataPath)
 
 objFunc = 'RMS';
 
@@ -40,18 +40,19 @@ optDistAll = [];
 rgbAll = [];
 
 for k = 1:length(blockNumAll)
-    AFCp = ARCloadFileBVAMS(subjNum+10,blockNumAll(k));
+    AFCp = ARCloadFileBVAMS(subjNum+10,blockNumAll(k),dataPath);
     optDistAll = [optDistAll; AFCp.meanv00./1.2255];
     rgbAll = [rgbAll; AFCp.rgb100];
     for l = 1:length(trialNumAll)
         % LOAD ZERNIKE TABLE AND TIMESTAMPS
-        [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumAll(k),trialNumAll(l),0);
+        [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumAll(k),trialNumAll(l),0,dataPath);
 
         NumCoeffs = width(ZernikeTable)-8; % determine how many coefficients are in the cvs file. 
         c=zeros(size(ZernikeTable,1),65); %this is the vector that contains the Zernike polynomial coefficients. We can work with up to 65. 
         PARAMS = struct;
-        PARAMS.PupilSize=mean(table2array(ZernikeTable(:,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
-        PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
+        indBadPupil = table2array(ZernikeTable(:,5))==0;
+        PARAMS.PupilSize=mean(table2array(ZernikeTable(~indBadPupil,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
+        PARAMS.PupilFitSize=mean(table2array(ZernikeTable(~indBadPupil,5))); 
         PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
         c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
         indBad = c(:,4)==0;
@@ -68,7 +69,12 @@ rgbUnq = unique(rgbAll,'rows');
 wLM = [0.5 1];
 wLprop = 0:0.025:1;
 
-coneWeightsFolder = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/coneWeightsErrorSpatFilter/colorMechPredictions/';
+if ispc
+   slash = '\';
+else
+   slash = '/';
+end
+coneWeightsFolder = [dataPath 'data' slash 'coneWeightsErrorSpatFilter' slash 'colorMechPredictions' slash];
 
 rgbLumNorm = [];
 rgbLumNorm(:,1) = (rgbUnq(:,1).^2.5)./0.2442;
@@ -101,7 +107,7 @@ for l = 1:length(wLM)
         wL = wLM(l)*wLprop(k);
         wM = -(wLM(l)-wL);
         wS = 0;
-        [~, defocus875mean, defocus875predTmp, rgbUnq, optDistUnq] = ARCtestWvInFocusMeanZstrehlPlotHelper(subjNum,defocus875,rgbAll,optDistAll,[wL wM wS]);
+        [~, defocus875mean, defocus875predTmp, rgbUnq, optDistUnq] = ARCtestWvInFocusMeanZstrehlPlotHelper(subjNum,defocus875,rgbAll,optDistAll,[wL wM wS],dataPath);
         optDistTag = imresize(optDistUnq',size(defocus875mean),'nearest');
         [pFit,RMSE(k)] = ARCfitLagLead(defocus875predTmp(:),defocus875mean(:),optDistTag(:),true,objFunc);
         if k==1
--- ARCimageQualityModels/ARCwvInFocusAll.m
+++ ARCimageQualityModels/ARCwvInFocusAll.m
@@ -30,7 +30,7 @@ else
 end
 foldername = [dataPath 'data' slash 'PresavedFigureData' slash];
 
-load([foldername 'wvMeanAndPredSfree.mat']);
+load([foldername 'wvMeanAndPredLminusM.mat']);
 
 symbDist = 'sod';
 conditionsOrderedNorm = [0.25 0.00 1.00; ...
@@ -45,6 +45,10 @@ conditionsOrderedNorm = [0.25 0.00 1.00; ...
                          1.00 0.50 0.25; ...
                          1.00 1.00 1.00];
 
+defocusDiffDue2LCA = [];
+predDiffDue2LCA = [];
+subjNum = [1 3 5 10 16 17 18 20];
+
 figure;
 set(gcf,'Position',[737 404 593 333]);
 subplot(1,2,1);
@@ -55,6 +59,10 @@ for i = 1:size(wvMeanAll,2)
     plot(mean(wvPredTmp(1:5,:),2),'k-','LineWidth',1);
     for j = 1:5
         errorbar(j,mean(wvMeanTmp(j,:),2),1.96.*std(wvPredTmp(j,:)')./sqrt(8),['k' symbDist(i)],'MarkerSize',10,'MarkerFaceColor',conditionsOrderedNorm(j,:),'LineWidth',1);
+        for k = 1:size(wvMeanTmp,2)
+            defocusDiffDue2LCA(i,j,k) = humanWaveDefocusARC(wvMeanTmp(1,k),wvMeanTmp(j,k),subjNum(k));
+            predDiffDue2LCA(i,j,k) = humanWaveDefocusARC(wvPredTmp(1,k),wvPredTmp(j,k),subjNum(k));
+        end
     end
 end
 % axis square;
@@ -83,6 +91,11 @@ set(gca,'XTick',1:5);
 set(gca,'XTickLabel',{'0.25' '0.50' '1.00' '2.00' '4.00'});
 xlabel('Red-blue ratio');
 
+defocusDiffDue2LCArb = mean(squeeze(defocusDiffDue2LCA(:,5,:)));
+predDiffDue2LCArb = mean(squeeze(predDiffDue2LCA(:,5,:)));
+
+defocusDiffDue2LCArbSE = 1.96.*std(defocusDiffDue2LCArb)./sqrt(8);
+predDiffDue2LCArbSE = 1.96.*std(predDiffDue2LCArb)./sqrt(8);
 %%
 
 load('/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/ARChroma/Meetings/Meeting_April_23/wvMeanAndPredLM.mat');
