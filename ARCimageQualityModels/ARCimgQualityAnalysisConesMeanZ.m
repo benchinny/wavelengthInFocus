d1e6b7a9361a57436f96b53d10c786f915e1eb6b
benchinny <ben.chinny@gmail.com>
2025-12-17 14:19:09 -0500

    clean up function for generating cone images

--- ARCimageQualityModels/ARCimgQualityAnalysisConesMeanZ.m
+++ ARCimageQualityModels/ARCimgQualityAnalysisConesMeanZ.m
@@ -18,7 +18,8 @@ d = displaySet(d,'ViewingDistance',1);
 % WHICH IS WHAT THE BVAMS HAS
 d = displaySet(d,'dpi',378); 
 
-bUseBVAMScal = 1; % if using BVAMS calibration data
+bUseBVAMScal = true; % if using BVAMS calibration data
+bPlotStim = false;
 
 if ispc
     slash = '\';
@@ -53,59 +54,44 @@ load T_xyz1931; % load color matching functions
 % DEFINE WAVELENGTH VECTOR: 380 TO 780 WITH 4NM INCREMENTS
 wave = S(1):S(2):S(1)+S(2)*(S(3)-1);
 
-%%
-
 if subjNum==3
    blockNums = 12:17;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==10
    blockNums = 3:8;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]']; 
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==1
    blockNums = 11:16;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]']; 
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==5
    blockNums = 3:8;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]']; 
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==9
    blockNums = 2:7;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]']; 
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==16
    blockNums = 2:7;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==17
    blockNums = 2:7;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==18
    blockNums = 2:7;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 elseif subjNum==20
    blockNums = 2:7;
-   trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
-   subjName = ['S' num2str(subjNum+10) '-OD'];
 end
+
+% ALL SUBJECTS HAD THE SAME NUMBER OF TRIALS
+trialNums = [[1:36]' [1:36]' [1:36]' [1:36]' [1:36]' [1:36]'];
+% FOR LOADING DATA LATER
+subjName = ['S' num2str(subjNum+10) '-OD'];
 
 %%
 
-cAll = [];
-optDistAll = [];
-rgbAll = [];
+cAll = []; % INITIALIZE MATRIX FOR STORING COEFFICIENTS
+optDistAll = []; % FOR CONCATENATING OPTICAL DISTANCES PER TRIAL
+rgbAll = []; % FOR CONCATENATING RGB VALUES PER TRIAL
 
 for l = 1:length(blockNums) % LOOP OVER BLOCK
-    blockNumInd = l;
-    blockNumTmp = blockNums(blockNumInd);
+    blockNumInd = l; % HELPS WITH READABILITY
+    blockNumTmp = blockNums(blockNumInd); % GRAB BLOCK
     AFCp = ARCloadFileBVAMS(subjNum+10,blockNumTmp,dataPath); % LOAD BVAMS DATA
-    rgbAll = [rgbAll; AFCp.rgb100];
+    rgbAll = [rgbAll; AFCp.rgb100]; % STACK COLOR CONDITIONS
     for k = 1:36 % LOOP OVER TRIAL
-        trialNumTmp = trialNums(k,blockNumInd);
+        trialNumTmp = trialNums(k,blockNumInd); % GRAB TRIAL NUMBERS
         
         % LOAD ZERNIKE TABLE AND TIMESTAMPS
         [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumTmp,trialNumTmp,0,dataPath);
@@ -113,34 +99,42 @@ for l = 1:length(blockNums) % LOOP OVER BLOCK
         NumCoeffs = width(ZernikeTable)-8; % determine how many coefficients are in the cvs file. 
         c=zeros(size(ZernikeTable,1),65); %this is the vector that contains the Zernike polynomial coefficients. We can work with up to 65. 
         PARAMS = struct;
-        PARAMS.PupilSize=mean(table2array(ZernikeTable(:,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
-        PARAMS.PupilSize = 4.*ones(size(PARAMS.PupilSize)); % hard code pupil size (FOR BVAMS ONLY)
-        PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
-        PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
+        indBadPupil = table2array(ZernikeTable(:,5))==0; % IDENTIFY BLINKS
+        % STORE PUPIL SIZE (SHOULD BE 4MM FOR EVERYTHING)
+        PARAMS.PupilSize=mean(table2array(ZernikeTable(~indBadPupil,5))); 
+        % GRAB ALL ZERNIKE COEFFICIENTS
         c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
+        % STACK OPTICAL DISTANCES. DIVIDE BY 1.2255 TO ACCOUNT FOR BVAMS
+        % CALIBRATION RELATING OPTOTUNE DEFOCUS CHANGE TO DEFOCUS AT EYE
         optDistTmp = (AFCp.meanv00(k)./1.2255).*ones([size(c,1) 1]);
         optDistAll = [optDistAll; optDistTmp];
-        cAll = [cAll; c];
+        cAll = [cAll; c]; % STORE ALL COEFFICIENTS
     end
 end
 
-indBad = cAll(:,4)==0;
+indBad = cAll(:,4)==0; % LOOK FOR BLINKS IN DEFOCUS ABERRATION VECTOR
 meanC = mean(cAll(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS
-rgb00 = unique(rgbAll,'rows');
+rgb00 = unique(rgbAll,'rows'); % GET UNIQUE COLOR CONDITIONS
 
-for k = 1 % LOOP OVER TRIAL
-    % recreate stimulus
+for k = 1:size(rgb00,1) % LOOP OVER COLOR CONDITIONS
+    % GET RGB VALUES FOR A PARTICULAR CONDITION
     rVal = rgb00(k,1);
     gVal = rgb00(k,2);
     bVal = rgb00(k,3);
+    % READ IN STIMULUS SPATIAL PATTERN
     im = imread([stimPath '/word_image_01.png']);
-    im = double(im);
+    im = double(im); % CONVERT TO DOUBLE FOR INCREASED PRECISION)
+    % IN THE IMAGE THE BLUE CHANNEL IS AT MAXIMUM VALUE, SO WE USE THIS
+    % PATTERN AS THE 'TEMPLATE'
     imPattern = squeeze(im(:,:,3));
+    % ADD ZERO PADDING (TO AVOID ARTIFACTS IN LATER ANALYSES)
     imPattern = [zeros([100 size(imPattern,2)]); imPattern; zeros([100 size(imPattern,2)])];
     imPattern = [zeros([size(imPattern,1) 30]) imPattern zeros([size(imPattern,1) 30])];
+    % NOW FILL IN ALL CHANNELS WITH SPATIAL PATTERN SCALED BY RGB VALUES
     I(:,:,3) = bVal.*imPattern;
     I(:,:,2) = gVal.*imPattern;
     I(:,:,1) = rVal.*imPattern;
+    % NORMALIZE TO 1 (WHAT ISETBIO EXPECTS)
     I = I./255;
     
     % Turn image into 'scene'
@@ -148,39 +142,61 @@ for k = 1 % LOOP OVER TRIAL
     % I think this copies the struct into an object
     vcAddObject(s); 
     
-    % figure; 
-    % set(gcf,'Position',[289 428 1056 420]);
-    % subplot(1,3,1);
-    % plot(d.wave,d.spd(:,1),'r','LineWidth',1.5); hold on;
-    % plot(d.wave,d.spd(:,2),'g','LineWidth',1.5);
-    % plot(d.wave,d.spd(:,3),'b','LineWidth',1.5);
-    % axis square;
-    % formatFigure('Wavelength (\lambda)','Radiance');
-    % subplot(1,3,2);
-    % imagesc(I);
-    % set(gca,'XTick',[]);
-    % set(gca,'YTick',[]);
-    % axis square;
-    % set(gca,'FontSize',15);
-    % title('Original');
-    % subplot(1,3,3);
-    % plot(s.spectrum.wave,squeeze(s.data.photons(160,160,:)),'-k','LineWidth',1);
-    % formatFigure('Wavelength (\lambda)','Photons');
-    % axis square;
+    % SOMETIMES MIGHT WANT TO PLOT THE STIMULUS TO MAKE SURE NOTHING GOT
+    % MESSED UP DURING THE STIMULUS CREATION PROCESS
+    if bPlotStim 
+        figure; 
+        set(gcf,'Position',[289 428 1056 420]);
+        subplot(1,3,1);
+        plot(d.wave,d.spd(:,1),'r','LineWidth',1.5); hold on;
+        plot(d.wave,d.spd(:,2),'g','LineWidth',1.5);
+        plot(d.wave,d.spd(:,3),'b','LineWidth',1.5);
+        axis square;
+        formatFigure('Wavelength (\lambda)','Radiance');
+        subplot(1,3,2);
+        imagesc(I);
+        set(gca,'XTick',[]);
+        set(gca,'YTick',[]);
+        axis square;
+        set(gca,'FontSize',15);
+        title('Original');
+        subplot(1,3,3);
+        plot(s.spectrum.wave,squeeze(s.data.photons(160,160,:)),'-k','LineWidth',1);
+        formatFigure('Wavelength (\lambda)','Photons');
+        axis square;
+    end
     
-    % Computing peak correlation for different wavelengths in focus
-    
+    % PEAK CORRELATION WILL BE COMPUTED FOR A RANGE OF 'WAVELENGTHS IN
+    % FOCUS'. THIS IS A DISTINCT VARIABLE FROM 'wave', WHICH IS THE
+    % WAVELENGTHS OVER WHICH THE STIMULUS IS DEFINED. I HAVE MADE THEM THE
+    % SAME, BUT THEY DON'T NECESSARILY HAVE TO BE (E.G. IF YOU JUST WANT TO
+    % LOOK WHAT HAPPENS WHEN A SPECIFIC WAVELENGTH IS IN FOCUS)
     wave2 = 380:4:780;
 
-    for i = 41
+    for i = 1:length(wave2) % LOOP OVER WAVELENGTHS IN FOCUS
+        % % IF YOU WANT TO COMPUTE FOR DIFFRACTION-LIMITED SCENARIO 
         % zCoeffs = [0 zeros(size(meanC(1:end-1)))];
+
+        % REFORMAT COEFFICIENTS (WAVEFRONT SENSOR LEAVES OUT THE 'PISTON'
+        % ZERNIKE TERM, WHICH IS ALWAYS 0)
         zCoeffs = [0 meanC(1:end-1)];
+        % CREATE ISETBIO WAVEFRONT OBJECT
         wvfP = wvfCreate('calc wavelengths', wave, ...
             'measured wavelength', wave2(i), ...
             'zcoeffs', zCoeffs, 'measured pupil', PARAMS.PupilSize, ...
             'name', sprintf('human-%d', PARAMS.PupilSize),'spatial samples',size(I,2));
+        % NOT SURE WHY PUPIL SIZE ALSO NEEDS TO BE SET HERE, BUT THE
+        % ISETBIO PSFS WON'T ALIGN WITH AUSTIN'S CODE UNLESS WE SET IT
+        % HERE, IF I RECALL CORRECTLY
         wvfP.calcpupilMM = PARAMS.PupilSize;
+        % SET CUSTOM LCA FUNCTION PER SUBJECT--ISETBIO WANTS IT TO BE SET
+        % IN A PARTICULAR FORMAT
         if subjNum==1
+            % CALCULATE MINIMUM AND MAXIMUM DEFOCUS FROM LCA FOR A
+            % PARTICULAR SUBJECT FOR THE RANGE OF WAVELENGTHS TO ANALYZE.
+            % THIS WILL ENSURE THAT THE MESH OVER THE PUPIL FUNCTION SPANS
+            % A SUFFICIENT RANGE (refSizeOfFieldMM). IF DEFOCUS IS LARGE
+            % ENOUGH, THE RANGE NEEDS TO BE REDUCED.
             defocusFromLCA = max(abs([humanWaveDefocusS1(wave2(i),min(wave)) ...
                                       humanWaveDefocusS1(wave2(i),max(wave))]));
             wvfP = wvfSet(wvfP, 'customlca', @humanWaveDefocusS1);
@@ -219,29 +235,45 @@ for k = 1 % LOOP OVER TRIAL
         else
             error('Subject number with no LCA function?');
         end
+
+        % IF DEFOCUS IS LARGE ENOUGH, THE AREA OF THE PUPIL THE PSF IS
+        % CALCULATED FROM NEEDS TO BE REDUCED, OR YOU WILL END UP WITH A
+        % DEGENERATE PSF
         if defocusFromLCA<1
             wvfP.refSizeOfFieldMM = 12;
         else
             wvfP.refSizeOfFieldMM = 6;
         end
+        % SET THE COEFFICIENT ON DEFOCUS TO 0 SINCE WE ARE LOOPING OVER
+        % WAVELENGTHS IN FOCUS EXPLICITLY, NOT IMPLICITLY THROUGH THE
+        % DEFOCUS TERM
         wvfP = wvfSet(wvfP, 'zcoeff', 0, 'defocus');
         
-        % Convert to siData format as well as wavefront object
+        % MAKE POINT-SPREAD FUNCTION (siPSFData) AND WAVEFRONT STRUCT
         [siPSFData, wvfP] = wvf2SiPsfARC(wvfP,'showBar',false,'nPSFSamples',size(I,2),'umPerSample',1.1512); 
-        oi = wvf2oi(wvfP); % CONVERT TO OPTICS OBJECT
-        % PADDING PSF NECESSARY TO ENSURE SAME SIZE AS STIMULUS
+        oi = wvf2oi(wvfP); % CONVERT WAVEFRONT STRUCT TO OPTICS OBJECT
+        % NEED TO REMOVE PADDED ZEROS FROM PSF TO MAKE SAME SIZE AS
+        % STIMULUS IMAGE. THE LINES BELOW IDENTIFY THE 'GOOD INDICES', I.E.
+        % THE INDICES THAT AREN'T THE PADDED ZEROS TO BE REMOVED
         paddingXCpsf = round((size(siPSFData.psf,2)-size(s.data.photons,2))/2);
         paddingYRpsf = round((size(siPSFData.psf,1)-size(s.data.photons,1))/2);
         indNotPadded = {(paddingYRpsf+1):(size(siPSFData.psf,1)-paddingYRpsf) ...
                         (paddingXCpsf+1):(size(siPSFData.psf,2)-paddingXCpsf)};
         % I HAD TO WRITE NEW CODE TO SET PSF BECAUSE I COULDN'T FIGURE OUT
-        % HOW TO DO WHAT I WANTED WITHIN ISETBIO FRAMEWORK
-        oi.optics.OTF = [];
-        for j = 1:size(siPSFData.psf,3)
+        % HOW TO DO WHAT I WANTED WITHIN DEFAULT ISETBIO FRAMEWORK. THIS
+        % CODE WILL ONLY WORK WITH THE 'isetbioChinVersion' VERSION OF
+        % ISETBIO.
+        oi.optics.OTF = []; % INITIALIZE ARRAY FOR STORING OTFS
+        for j = 1:size(siPSFData.psf,3) % LOOP OVER WAVELENGTHS
+            % REPLACE OTF FIELD WITH A NEW OTF CALCULATED FROM THE PSFS WE
+            % JUST CALCULATED. COULDN'T FIGURE OUT HOW TO MAKE ISETBIO DO
+            % THIS AUTOMATICALLY, SO I DID IT MANUALLY (AND CHECKED THE
+            % OUTPUT!)
             oi.optics.OTF.OTF(:,:,j) = fft2(fftshift(squeeze(siPSFData.psf(indNotPadded{1},indNotPadded{2},j))));
         end
-        % oi = oiCreateARC('human',wave,Dall(i)); % create optics
-        oi = oiCompute(oi, s); % compute optical image of stimulus
+
+        % MAIN STEP: COMPUTE OPTICAL IMAGE OF STIMULUS
+        oi = oiCompute(oi, s); 
     
         % Create the coneMosaic object
         cMosaic = coneMosaic;
@@ -253,13 +285,16 @@ for k = 1 % LOOP OVER TRIAL
         absorptions = cMosaic.computeSingleFrame(oi, 'fullLMS', true);            
         
         display(['Peak correlation loop ' num2str(i) ' stimulus ' num2str(k)]);
+        % CONVERT TO SINGLE TO SAVE SPACE
         absorptions = single(absorptions);
+        % PLACE ABSORPTIONS IN STRUCT
         S = struct;
         S.absorptions = absorptions;
+        % NAME FOR SAVING
         fnameCone = ['subj' num2str(subjNum) 'stimulus' num2str(k) 'focusInd' num2str(i)];
+        % UNCOMMENT LINE BELOW TO SAVE NEW CONE IMAGES
         % save([savePath num2str(subjNum) '/' fnameCone '.mat'],"-fromstruct",S);
     end
 end
 
 end
-% save('/Users/benjaminchin/Documents/ARchromaScraps/ARCmodelOutput2_5.mat','meanv00all','wvInFocus1all','rgb1all','defocusBasic');
