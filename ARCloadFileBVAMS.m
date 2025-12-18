4cf8203d21f7d9d8a8711b82fa06e5c6c9ee105b
benchinny <ben.chinny@gmail.com>
2025-12-18 14:00:20 -0500

    clean up loading functions (will need to check all functions that use them for correct calls)

--- ARCloadFileBVAMS.m
+++ ARCloadFileBVAMS.m
@@ -4,20 +4,21 @@ function AFCp = ARCloadFileBVAMS(subjNum,blockNum,dataPath)
 % HAPPENS HERE IS SPECIFIC TO THE NAMING AND ORGANIZATION CONVENTIONS OF
 % THE DATA
 
-AFCp = [];
-if ispc
-    slash = '\';
-else
-    slash = '/';
-end
-dataFolder = [dataPath 'data' slash 'ARC' slash];
+% INITIALIZE SOMETHING TO RETURN EVEN IF NOTHING IS FOUND
+AFCp = []; 
 
-load([dataFolder 'AFCflsR']);
+dataFolder = fullfile(dataPath,'data','ARC');
 
+% LOAD 'MASTER FILE' SHOWING ALL FILE NAMES FOR EACH BLOCK AND CONDITION
+load(fullfile(dataFolder,'AFCflsR'));
+
+% FIND THE FILE NAME FOR THE BLOCK AND CONDITION IN QUESTION
 filenameTmp = AFCfls{subjNum,blockNum};
 
+% GRAB THE PART OF THE FILE NAME THAT DOESN'T INCLUDE THE PATH ON THE BVAMS
+% LOCAL MACHINE
 filenameTmp = filenameTmp((length('H:\Shared drives\CIVO_BVAMS\data\ARC\')+1):end);
 
-load([dataFolder filenameTmp]);
+load(fullfile(dataFolder,filenameTmp));
 
 end
