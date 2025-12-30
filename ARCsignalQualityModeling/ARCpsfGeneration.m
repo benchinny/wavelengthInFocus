6e8de38bdd8be8f6fa99633528855f643aa9cbba
benchinny <ben.chinny@gmail.com>
2025-12-30 08:22:33 -0500

    current state of PSF generation code

--- ARCsignalQualityModeling/ARCpsfGeneration.m
+++ ARCsignalQualityModeling/ARCpsfGeneration.m
@@ -8,15 +8,13 @@ ieInit;
 
 % WHETHER OR NOT TO PLOT STIMULUS
 bPlotStim = false;
-% WHETHER OR NOT TO SAVE CONE IMAGES
+% WHETHER OR NOT TO SAVE POINT-SPREAD FUNCTIONS
 bSave = true;
-% WHETHER OR NOT TO APPLY OPTICS
-bApplyOptics = true;
 
 %% Set up display struct and build Ben's stimulus
 
 % PATH TO SAVE
-savePath = fullfile(dataPath,'data','coneImages');
+savePath = fullfile(dataPath,'data','psfs');
 
 % DEFINE LIGHT WAVELENGTHS TO SAMPLE STIMULUS AT
 wave = 380:4:780;
@@ -30,63 +28,48 @@ d = ARCmodelDispSetup(dataPath,0);
 
 rgb00 = unique(rgbAll,'rows'); % GET UNIQUE COLOR CONDITIONS
 
-for k = 1:size(rgb00,1) % LOOP OVER COLOR CONDITIONS
-    % MAKE STIMULUS USING COMMON HELPER FUNCTION
-    [s, ~] = ARCmodelStimSetup(dataPath,subjNum,'accommodation',d,rgb00(k,:),bPlotStim);
-    % PEAK CORRELATION WILL BE COMPUTED FOR A RANGE OF 'WAVELENGTHS IN
-    % FOCUS'. THIS IS A DISTINCT VARIABLE FROM 'wave', WHICH IS THE
-    % WAVELENGTHS OVER WHICH THE STIMULUS IS DEFINED. I HAVE MADE THEM THE
-    % SAME, BUT THEY DON'T NECESSARILY HAVE TO BE (E.G. IF YOU JUST WANT TO
-    % LOOK AT WHAT HAPPENS WHEN A SPECIFIC WAVELENGTH IS IN FOCUS)
-    wave2 = 380:4:780;
+% STIM COLOR DOESN'T REALLY MATTER HERE--WE JUST WANT THE STIMULUS TO
+% FIGURE OUT HOW TO SIZE THE PSF
+k = 12;
+% MAKE STIMULUS USING COMMON HELPER FUNCTION
+[s, ~] = ARCmodelStimSetup(dataPath,subjNum,'accommodation',d,rgb00(k,:),bPlotStim);
+% PEAK CORRELATION WILL BE COMPUTED FOR A RANGE OF 'WAVELENGTHS IN
+% FOCUS'. THIS IS A DISTINCT VARIABLE FROM 'wave', WHICH IS THE
+% WAVELENGTHS OVER WHICH THE STIMULUS IS DEFINED. I HAVE MADE THEM THE
+% SAME, BUT THEY DON'T NECESSARILY HAVE TO BE (E.G. IF YOU JUST WANT TO
+% LOOK AT WHAT HAPPENS WHEN A SPECIFIC WAVELENGTH IS IN FOCUS)
+wave2 = 380:4:780;
 
-    parfor i = 1:length(wave2) % LOOP OVER WAVELENGTHS IN FOCUS
-        % % IF YOU WANT TO COMPUTE FOR DIFFRACTION-LIMITED SCENARIO 
-        % zCoeffs = [0 zeros(size(meanC(1:end-1)))];
+parfor i = 1:length(wave2) % LOOP OVER WAVELENGTHS IN FOCUS
+    % % IF YOU WANT TO COMPUTE FOR DIFFRACTION-LIMITED SCENARIO 
+    % zCoeffs = [0 zeros(size(meanC(1:end-1)))];
 
-        % REFORMAT COEFFICIENTS (WAVEFRONT SENSOR LEAVES OUT THE 'PISTON'
-        % ZERNIKE TERM, WHICH IS ALWAYS 0)
-        zCoeffs = [0 meanC(1:end-1)];
-        % NUMBER OF SPATIAL SAMPLES IN X AND Y (NOTE: NOT ROWS AND COLUMNS!)
-        spatialSamplesXY = [size(s.data.photons,2) size(s.data.photons,1)];
-        % FIX DEFOCUS TERM TO 0 BECAUSE WE ARE MODELING WAVELENGTH IN FOCUS
-        defocusSet = 0;
-        % CALL HELPER FUNCTION FOR SETTING UP OPTICS STRUCT SPECIFIC TO
-        % MODELING FOR THIS PROJECT
-        oi = ARCmodelOpticsSetup(subjNum,zCoeffs,wave,wave2(i),PupilSize,spatialSamplesXY,defocusSet);
+    % REFORMAT COEFFICIENTS (WAVEFRONT SENSOR LEAVES OUT THE 'PISTON'
+    % ZERNIKE TERM, WHICH IS ALWAYS 0)
+    zCoeffs = [0 meanC(1:end-1)];
+    % NUMBER OF SPATIAL SAMPLES IN X AND Y (NOTE: NOT ROWS AND COLUMNS!)
+    spatialSamplesXY = [size(s.data.photons,2) size(s.data.photons,1)];
+    % FIX DEFOCUS TERM TO 0 BECAUSE WE ARE MODELING WAVELENGTH IN FOCUS
+    defocusSet = 0;
+    % CALL HELPER FUNCTION FOR SETTING UP OPTICS STRUCT SPECIFIC TO
+    % MODELING FOR THIS PROJECT
+    oi = ARCmodelOpticsSetup(subjNum,zCoeffs,wave,wave2(i),PupilSize,spatialSamplesXY,defocusSet);
+    
+    display(['Peak correlation loop ' num2str(i)]);
 
-        if bApplyOptics % IF APPLYING OPTICS
-           % MAIN STEP: COMPUTE OPTICAL IMAGE OF STIMULUS
-           oi = oiCompute(oi, s); 
-           % NO EXTRA TAGS IN FILE NAME
-           opticsNameTag = '';
-        else % IF NOT APPLYING OPTICS
-           % MAIN STEP: COMPUTE OPTICAL IMAGE OF STIMULUS
-           oi = oiComputeNoOptics(oi,s);
-           % TAG FILENAME TO INDICATE NO OPTICS WERE USED
-           opticsNameTag = 'noOptics';
-        end
-
-        % Create the coneMosaic object
-        cMosaic = coneMosaic;
-        % Set size to show relevant portion of scene
-        cMosaic.setSizeToFOV(1 * sceneGet(s, 'fov'));
-
-        % key line for computing absorptions
-        absorptions = cMosaic.computeSingleFrame(oi, 'fullLMS', true);            
-        
-        display(['Peak correlation loop ' num2str(i) ' stimulus ' num2str(k)]);
-        % CONVERT TO SINGLE TO SAVE SPACE
-        absorptions = single(absorptions);
-        % PLACE ABSORPTIONS IN STRUCT
-        S = struct;
-        S.absorptions = absorptions;
-        % NAME FOR SAVING
-        fnameCone = ['subj' num2str(subjNum) 'stimulus' num2str(k) 'focusInd' num2str(i) opticsNameTag];
-        % UNCOMMENT LINE BELOW TO SAVE NEW CONE IMAGES
-        if bSave
-           save(fullfile(savePath,['S' num2str(subjNum)],[fnameCone '.mat']),"-fromstruct",S);
-        end
+    % PLACE ABSORPTIONS IN STRUCT
+    S = struct;
+    S.psf = single(siPSFData.psf(134:184,134:184,:));
+    OTFzeroCentered = [];
+    for j = 1:size(siPSFData.psf,3)
+        OTFzeroCentered(:,:,j) = ifftshift(squeeze(oi.optics.OTF.OTF(:,:,j)));
+    end
+    S.otf = single(OTFzeroCentered(116:175,130:189,:));    
+    % NAME FOR SAVING
+    fnameCone = ['subj' num2str(subjNum) 'PSFfocusInd' num2str(i)];
+    % UNCOMMENT LINE BELOW TO SAVE NEW CONE IMAGES
+    if bSave
+       save(fullfile(savePath,['S' num2str(subjNum)],[fnameCone '.mat']),"-fromstruct",S);
     end
 end
 
