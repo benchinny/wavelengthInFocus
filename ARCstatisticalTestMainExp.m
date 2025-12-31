6682a96e609758af1023194c3e240bf76353daec
benchinny <ben.chinny@gmail.com>
2025-12-31 16:50:02 -0500

    reintegrate script for running statistical tests

--- ARCstatisticalTestMainExp.m
+++ ARCstatisticalTestMainExp.m
@@ -1,78 +1,69 @@
 %% JUST SETTING UP PATHS TO PRE-SAVED DATA
 
 dataPath = 'C:\Users\bmccis\OneDrive - rit.edu\Documents\wavelengthInFocusData\';
-if ispc
-    slash = '\';
-else
-    slash = '/';
-end
 
-load([dataPath 'data' slash 'PresavedFigureData' slash 'allExp1DataRGB.mat']);
+load(fullfile(dataPath,'data','PresavedFigureData','allExp1DataRGB.mat'));
 
 %% CREATE COLUMNS FOR TABLE FOR LINEAR MODEL
 
-% CODING EACH PARTICIPANT NUMBER AS A LETTER SO MATLAB DOESN'T INTERPRET
-% PARTICIPANT NUMBERS AS CONTINUOUS NUMERIC DATA
-subjStr = 'A B C    D     EFG H';
+subjNumTagOptions = 'ABCDEFGH';
+% INDICES OF PRESAVED DATA THAT CORRESPOND TO CONDITIONS WITH NO GREEN IN
+% THEM
+indGreenless = 1:5;
+% INDICES OF PRESAVED DATA THAT CORRESPOND TO CONDITIONS WITH SOME GREEN IN
+% THEM
+indGreen = 6:10;
 
-rgbLumNormCndUnq = unique(rgbLumNormCndAll,'rows'); % UNIQUE CONDITIONS
-indGreenless = [1 3 7 6 5]; % INDICES FOR ORDERING BLUEST TO REDDEST
-indGreen = [2 4 10 9 8]; % SAME AS ABOVE BUT FOR 'SOME GREEN' CONDITIONS
+% FIRST MAKE APPROPRIATE COLUMN VECTORS FOR CONDITIONS WITH SOME GREEN IN
+% THEM
 
-rbRatioGreenless = []; % RED BLUE RATIO
-optDistCndGreenless = []; % STIMULUS DISTANCE
-subjNumTagGreenless = []; % SUBJECT NUMBER TAG
-wvInFocusMeanGreenless = []; % WAVELENGTH IN FOCUS
+optDistCndGreen = []; % STIMULUS DISTANCES
+wvInFocusMeanGreen = []; % WAVELENGTHS IN FOCUS
+subjNumTagGreen = []; % ALPHABETICAL TAG FOR EACH SUBJECT
+rbRatioGreen = []; % RED-BLUE RATIO
 
-% THIS LOOP IS JUST SORTING DATA INTO COLUMNS OF TABLE--SEE 'dataTable'
-% VARIABLES IN NEXT SECTION
-for i = 1:length(indGreenless) 
-    % FOR EACH COLOR, GET THE MEAN ACCOMMODATIVE RESPONSE FOR EACH
-    % PARTICIPANT AND OPTICAL DISTANCE
-    rgbTmp = rgbLumNormCndUnq(indGreenless(i),:); 
-    indRgbTmp = find(abs(rgbLumNormCndAll(:,1)-rgbTmp(1))<0.001 & ... 
-                     abs(rgbLumNormCndAll(:,2)-rgbTmp(2))<0.001 & ... 
-                     abs(rgbLumNormCndAll(:,3)-rgbTmp(3))<0.001);
-    for j = 1:length(indRgbTmp) % ADD TO VECTOR
-        rbRatioGreenless(end+1) = rgbLumNormCndAll(indRgbTmp(j),1)/rgbLumNormCndAll(indRgbTmp(j),3);
-        optDistCndGreenless(end+1) = optDistCndAll(indRgbTmp(j));
-        subjNumTagGreenless = [subjNumTagGreenless; subjStr(subjNumTag(indRgbTmp(j)))];
-        wvInFocusMeanGreenless(end+1) = mean(wvInFocusCellAll{indRgbTmp(j)});
+for i = 1:length(subjNumTagOptions) % LOOP OVER SUBJECTS
+    for j = 1:size(optDistCndAll,1) % LOOP OVER DISTANCES
+        wvInFocusMeanGreen = [wvInFocusMeanGreen; squeeze(wvMeanAll(indGreen,j,i))];
+        % GRAB COLOR CONDITIONS FIRST
+        conditionsOrderedNormTmp = squeeze(conditionsOrderedNormAll(indGreen,:,i));
+        rbRatioGreen = [rbRatioGreen; conditionsOrderedNormTmp(:,1)./conditionsOrderedNormTmp(:,3)];
+        optDistCndGreen = [optDistCndGreen; optDistCndAll(j,i).*ones([length(indGreen) 1])];
+        subjNumTagGreen = [subjNumTagGreen; repmat(subjNumTagOptions(i),[length(indGreen) 1])];
     end
 end
 
-% SAME AS ABOVE LOOP, JUST FOR STIMULI WITH GREEN PRIMARY ON
-rbRatioGreen = [];
-optDistCndGreen = [];
-subjNumTagGreen = [];
-wvInFocusMeanGreen = [];
+% MAKE APPROPRIATE COLUMN VECTORS FOR CONDITIONS WITH NO GREEN IN THEM
 
-for i = 1:length(indGreen) 
-    rgbTmp = rgbLumNormCndUnq(indGreen(i),:); 
-    indRgbTmp = find(abs(rgbLumNormCndAll(:,1)-rgbTmp(1))<0.001 & ... 
-                     abs(rgbLumNormCndAll(:,2)-rgbTmp(2))<0.001 & ... 
-                     abs(rgbLumNormCndAll(:,3)-rgbTmp(3))<0.001);
-    for j = 1:length(indRgbTmp)
-        rbRatioGreen(end+1) = rgbLumNormCndAll(indRgbTmp(j),1)/rgbLumNormCndAll(indRgbTmp(j),3);
-        optDistCndGreen(end+1) = optDistCndAll(indRgbTmp(j));
-        subjNumTagGreen = [subjNumTagGreen; subjStr(subjNumTag(indRgbTmp(j)))];
-        wvInFocusMeanGreen(end+1) = mean(wvInFocusCellAll{indRgbTmp(j)});
+optDistCndGreenless = []; % STIMULUS DISTANCES
+wvInFocusMeanGreenless = []; % WAVELENGTHS IN FOCUS
+subjNumTagGreenless = []; % ALPHABETICAL TAG FOR EACH SUBJECT
+rbRatioGreenless = []; % RED-BLUE RATIO
+
+for i = 1:length(subjNumTagOptions) % LOOP OVER SUBJECTS
+    for j = 1:size(optDistCndAll,1) % LOOP OVER DISTANCES
+        wvInFocusMeanGreenless = [wvInFocusMeanGreenless; squeeze(wvMeanAll(indGreenless,j,i))];
+        % GRAB COLOR CONDITIONS FIRST
+        conditionsOrderedNormTmp = squeeze(conditionsOrderedNormAll(indGreenless,:,i));
+        rbRatioGreenless = [rbRatioGreenless; conditionsOrderedNormTmp(:,1)./conditionsOrderedNormTmp(:,3)];
+        optDistCndGreenless = [optDistCndGreenless; optDistCndAll(j,i).*ones([length(indGreenless) 1])];
+        subjNumTagGreenless = [subjNumTagGreenless; repmat(subjNumTagOptions(i),[length(indGreenless) 1])];
     end
 end
 
 %% CREATE TABLES
 
-dataTableGreen = array2table([optDistCndGreen' wvInFocusMeanGreen'], ...
+dataTableGreen = array2table([optDistCndGreen wvInFocusMeanGreen], ...
                              'VariableNames',{'OpticalDistance' 'MeanD'});
 
 dataTableGreen.("Subject") = subjNumTagGreen;
-dataTableGreen.("ColorRatio") = rbRatioGreen';
+dataTableGreen.("ColorRatio") = rbRatioGreen;
 
-dataTableGreenless = array2table([optDistCndGreenless' wvInFocusMeanGreenless'], ...
+dataTableGreenless = array2table([optDistCndGreenless wvInFocusMeanGreenless], ...
                              'VariableNames',{'OpticalDistance' 'MeanD'});
 
 dataTableGreenless.("Subject") = subjNumTagGreenless;
-dataTableGreenless.("ColorRatio") = rbRatioGreenless';
+dataTableGreenless.("ColorRatio") = rbRatioGreenless;
 
 %% RUN GLME
 
