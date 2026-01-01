78dd99c4fd5c6249aab6f1d74eb09d58af5bf51e
benchinny <ben.chinny@gmail.com>
2026-01-01 09:30:17 -0500

    add code for calculating gain stats

--- ARCnlz_Fig3and4stats.m
+++ ARCnlz_Fig3and4stats.m
@@ -6,15 +6,30 @@ foldername = fullfile(dataPath,'data','PresavedFigureData');
 % PRESAVED DATA OPTIONS:
 load(fullfile(foldername,'allExp1DataRGB.mat'));
 
+optDistUnq = [1.5; 2.5; 3.5];
+
 % REARRANGE DEFOCUS AT 555NM VECTOR
 dfMean555avgColor = squeeze(mean(dfMean555all(1:10,:,:)));
 % CALCULATE LAGS AND LEADS
-lagLead = bsxfun(@plus,dfMean555avgColor,[1.5; 2.5; 3.5]);
+lagLead = bsxfun(@plus,dfMean555avgColor,optDistUnq);
 % COMPUTE DIFFERENCE BETWEEN LAG/LEAD AT 3.5D AND LAG/LEAD AT 1.5D
 lagLeadDiff = lagLead(3,:)-lagLead(1,:);
 % CALCULATE MEAN AND STD ERROR
 meanLagLeadDiff = mean(lagLeadDiff);
 SElagLeadDiff = 1.96.*std(lagLeadDiff)./sqrt(8);
+
+% CALCULATING AVERAGE RESPONSE GAIN
+for i = 1:size(dfMean555avgColor,2)
+    % MAKE REGRESSOR VECTOR FOR INTERCEPT AND SLOPE
+    X = [ones(length(optDistUnq),1) optDistUnq];
+    % LINEAR REGRESS USING MATLAB BACKSLASH OPERATOR (DOCUMENTED ON
+    % MATHWORKS WEBSITE)
+    b(:,i) = X\(-dfMean555avgColor(:,i));
+end
+
+% MEAN AND SE OF GAIN
+meanGain = mean(b(2,:));
+SEgain = 1.96.*std(b(2,:))./sqrt(8);
 
 %% STATISTICS FOR FIGURE 4
 
