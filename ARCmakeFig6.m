79b858e770490a03fc4d4f6ef3cb4daa5b8a7815
benchinny <ben.chinny@gmail.com>
2026-01-01 21:02:29 -0500

    clean up / add comments to script for making Figure 6

--- ARCmakeFig6.m
+++ ARCmakeFig6.m
@@ -1,82 +1,89 @@
-%% RECORD OF BEST FIT CONE WEIGHTS
+%% SCRIPT FOR MAKING FIGURE 6 (ILLUSTRATE MODEL INTUITION)
 
 dataPath = 'C:\Users\bmccis\OneDrive - rit.edu\Documents\wavelengthInFocusData\';
-subjNumInd = 2;
-subjNumAll = [1 3 5 10 16 17 18 20];
+subjNumInd = 2; % SUBJECT TO LOOK AT
+% FOLDER NAME WITH PRESAVED DATA
+foldername = fullfile(dataPath,'data','PresavedFigureData');
 
-LMSweights = [0.5467    0.2533   -1.0000; ...
-              0.6475    0.4025   -1.0000; ...
-              0.7650    0.1350   -0.5000; ...
-              1.1475    0.2025   -1.0000; ...
-              0.2267    0.5733   -1.0000; ...
-              0.2850    0.6150   -1.0000; ...
-              1.1050    0.1950   -1.0000; ...
-              1.1433    0.2567   -0.2500];
+% LIST OF ALL SUBJECTS
+subjNumAll = [1 3 5 10 16 17 18 20]; 
+% WEIGHTS ON S FOR EACH SUBJECT (REFLECTED IN OTHER CODE)
+wS = [-1 -1 -0.5 -1 -1 -1 -1 -0.25];
 
-LMweights = [0.8250    0.1750         0; ...
-             0.8500    0.1500         0; ...
-             0.8500    0.1500         0; ...
-             0.8500    0.1500         0; ...
-             0.2500    0.7500         0; ...
-             0.8250    0.1750         0; ...
-             0.8500    0.1500         0; ...
-             0.7500    0.2500         0];
+% LOAD WEIGHTS FROM PRESAVED DATA
 
-LminusMweights = [0.3250   -0.1750         0; ...
-                  0.3750   -0.1250         0; ...
-                  0.4125   -0.0875         0; ...
-                  0.8000   -0.2000         0; ...
-                  0.7000   -0.3000         0; ...
-                  0.4000   -0.1000         0; ...
-                  0.6250   -0.3750         0; ...
-                  0.4875   -0.0125         0];
+% FOR L+M
+load(fullfile(foldername,'wvMeanAndPredLM.mat'),'wLpropMinAll','wLMminAll');
+wL = wLMminAll(2)*wLpropMinAll(2);
+wM = wLMminAll(2)-wL;
+LMweights = [wL wM 0];
 
-% MAKE EXAMPLE CONE IMAGES FOR SUBJECT 2 AT INFORMATIVE WAVELENGTHS
+% FOR BLUE-YELLOW
+load(fullfile(foldername,'wvMeanAndPredLMS.mat'),'wLpropMinAll','wLMminAll');
+wL = wLMminAll(2)*wLpropMinAll(2);
+wM = wLMminAll(2)-wL;
+LMSweights = [wL wM wS(subjNumInd)];
 
-wave = 380:4:780;
-wv2vis = [616 552 468];
-stimInd = [1 8 6];
-coneImgFilteredEgLMS = [];
-coneImgFilteredEgLM = [];
-coneImgFilteredEgLminusM = [];
-absorptionsS_LMS = [];
-absorptionsLM_LMS = [];
+% FOR RED-GREEN
+load(fullfile(foldername,'wvMeanAndPredLminusM.mat'),'wLpropMinAll','wLMminAll');
+wL = wLMminAll(2)*wLpropMinAll(2);
+wM = wLMminAll(2)-wL;
+LminusMweights = [wL wM 0];
 
-peakCorrAll = [];
-for i = 1:length(stimInd)
-    for j = 1:length(wv2vis)
-        [wvInFocus, coneImgFilteredEg, coneImgOrigFilteredEg, wvInFocus2, wave, peakCorr, absorptions] = ...
-         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LMSweights(subjNumInd,:),find(wave==wv2vis(j)),dataPath);
-         coneImgFilteredEgLMS(:,:,i,j) = coneImgFilteredEg;
+%% MAKE EXAMPLE CONE IMAGES FOR SUBJECT 2 AT INFORMATIVE WAVELENGTHS
+
+wave = 380:4:780; % WAVELENGTHS TO SAMPLE AT
+wv2vis = [616 552 468]; % WAVELENGTHS TO VISUALIZE
+stimInd = [1 8 6]; % STIMULUS INDICES CORRESPONDING TO STIMULI OF INTEREST
+coneImgEgLMS = []; % FOR SAVING OUT CONE IMAGES FOR LMS
+coneImgEgLM = []; % FOR SAVING OUT CONE IMAGES FOR LM
+coneImgEgLminusM = []; % FOR SAVING OUT CONE IMAGES FOR L-M
+absorptionsS_LMS = []; % FOR SAVING OUT S-CONE IMAGE
+absorptionsLM_LMS = []; % FOR SAVING OUT L+M IMAGE
+
+peakCorrAll = []; % ARRAY FOR STORING SIGNAL QUALITY
+for i = 1:length(stimInd) % LOOP OVER STIMULUS INDICES
+    for j = 1:length(wv2vis) % LOOP OVER WAVELENGTHS TO VISUALIZE
+        % SAVE OUT IMAGES FOR BLUE-YELLOW
+        [~, coneImgEg, ~, wave, ~, absorptions] = ...
+         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LMSweights,find(wave==wv2vis(j)),dataPath);
+         coneImgEgLMS(:,:,i,j) = coneImgEg;
          absorptionsS_LMS(:,:,i,j) = absorptions(:,:,3);
          absorptionsLM_LMS(:,:,i,j) = absorptions(:,:,1)+absorptions(:,:,2);
-
-        [wvInFocus, coneImgFilteredEg, coneImgOrigFilteredEg, wvInFocus2, wave, peakCorr, absorptions] = ...
-         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LMSweights(subjNumInd,1) LMSweights(subjNumInd,2) 0],find(wave==wv2vis(j)),dataPath); 
-        coneImgFilteredEgLM(:,:,i,j) = coneImgFilteredEg;
-
-        [wvInFocus, coneImgFilteredEg, coneImgOrigFilteredEg, wvInFocus2, wave, peakCorr, absorptions] = ...
-         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LminusMweights(subjNumInd,1) LminusMweights(subjNumInd,2) 0],find(wave==wv2vis(j)),dataPath); 
-        coneImgFilteredEgLminusM(:,:,i,j) = coneImgFilteredEg;        
+        
+        % SAVE OUT IMAGES FOR L+M
+        [~, coneImgEg, ~, wave, ~, absorptions] = ...
+         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LMSweights(1) LMSweights(2) 0],find(wave==wv2vis(j)),dataPath); 
+        coneImgEgLM(:,:,i,j) = coneImgEg;
+        
+        % SAVE OUT IMAGES FOR L-M
+        [~, coneImgEg, ~, wave, ~, absorptions] = ...
+         ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LminusMweights,find(wave==wv2vis(j)),dataPath); 
+        coneImgEgLminusM(:,:,i,j) = coneImgEg;        
     end
-    [~, ~, ~, ~, wave, peakCorr, ~] = ...
-    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LMSweights(subjNumInd,1) LMSweights(subjNumInd,2) LMSweights(subjNumInd,3)],1:101,dataPath);
+    % CALCULATE SIGNAL QUALITY VS. WAVELENGTH IN FOCUS FOR BLUE-YELLOW
+    [~, ~, ~, ~, peakCorr, ~] = ...
+    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LMSweights,1:101,dataPath);
     peakCorrAll(:,end+1) = peakCorr;
-    [~, ~, ~, ~, wave, peakCorr, ~] = ...
-    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LMweights(subjNumInd,1) LMweights(subjNumInd,2) 0],1:101,dataPath);
+    % CALCULATE SIGNAL QUALITY VS. WAVELENGTH IN FOCUS FOR LUMINANCE
+    [~, ~, ~, ~, peakCorr, ~] = ...
+    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LMweights,1:101,dataPath);
     peakCorrAll(:,end+1) = peakCorr;
-    [~, ~, ~, ~, wave, peakCorr, ~] = ...
-    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),[LminusMweights(subjNumInd,1) LminusMweights(subjNumInd,2) LminusMweights(subjNumInd,3)],1:101,dataPath);
+    % CALCULATE SIGNAL QUALITY VS. WAVELENGTH IN FOCUS FOR RED-GREEN
+    [~, ~, ~, ~, peakCorr, ~] = ...
+    ARCwvInFocusConesMeanZsandbox(subjNumAll(subjNumInd),stimInd(i),LminusMweights,1:101,dataPath);
     peakCorrAll(:,end+1) = peakCorr;
 end
 
 %% PLOT DIFFERENT CONE IMAGES FOR BUILDING MODEL INTUITIONS
-absorptionsS_LMSplot = absorptionsS_LMS.*1;
-absorptionsLM_LMSplot = absorptionsLM_LMS.*1;
-coneImgFilteredEgLMSplot = coneImgFilteredEgLMS.*1;
+
+% SPECIFY WHICH IMAGES TO PLOT
+absorptionsS_LMSplot = absorptionsS_LMS;
+absorptionsLM_LMSplot = absorptionsLM_LMS;
+coneImgEgLMSplot = coneImgEgLMS;
 redblue = make_yellow_blue_colormap(1);
-axisLims = [min([coneImgFilteredEgLM(:); coneImgFilteredEgLMS(:)]) ...
-            max([coneImgFilteredEgLM(:); coneImgFilteredEgLMS(:)])];
+axisLims = [min([coneImgEgLM(:); coneImgEgLMS(:)]) ...
+            max([coneImgEgLM(:); coneImgEgLMS(:)])];
 stimCell = {'More blue' 'Purple' 'More red'};
 bColorBar = false;
 
@@ -84,7 +91,7 @@ figure;
 set(gcf,'Position',[29 90 890 908]);
 for i = 1:length(wv2vis)
     subplot(3,3,(i-1)*3+3);
-    imagesc(squeeze(coneImgFilteredEgLMSplot(:,:,1,i))); 
+    imagesc(squeeze(coneImgEgLMSplot(:,:,1,i))); 
     axis square; 
     set(gca,'XTick',[]);
     set(gca,'YTick',[]);
@@ -133,7 +140,7 @@ figure;
 set(gcf,'Position',[29 90 890 908]);
 for i = 1:length(wv2vis)
     subplot(3,3,(i-1)*3+3);
-    imagesc(squeeze(coneImgFilteredEgLMSplot(:,:,3,i))); 
+    imagesc(squeeze(coneImgEgLMSplot(:,:,3,i))); 
     axis square; 
     set(gca,'XTick',[]);
     set(gca,'YTick',[]);
@@ -177,42 +184,3 @@ for i = 1:length(wv2vis)
        title('L+M image');
     end
 end
-
-%% FIRST FINCH PLOT
-
-dataFolder = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/ARChroma/Meetings/Meeting_April_23/';
-FinchData = readtable([dataFolder 'Finch_data_RedVsGreenErrorBars' '.csv']);
-lumRatio = 0:0.125:1;
-accommodation = [0.179 0.144 0.106 0.055 0.029 0.025 -0.011 -0.072 -0.175];
-ubAcc = [0.204 0.172 0.135 0.0844 0.057 0.0532 0.0128 -0.0443 -0.149];
-lbAcc = [0.149 0.120 0.075 0.0285 -0.0028 -0.0046 -0.0367 -0.0979 -0.202];
-figure;
-errorbar(lumRatio,accommodation,ubAcc-accommodation,accommodation-lbAcc,'ko','LineWidth',1,'MarkerSize',10,'MarkerFaceColor','k');
-hold on;
-plot(xlim,0.273.*[1 1],'r-','LineWidth',1);
-plot(xlim,-0.335.*[1 1],'g-','LineWidth',1);
-axis square;
-set(gca,'FontSize',15);
-xlabel('Green/Red Luminance');
-ylabel('Accommodation');
-ylim([-1 0.3]);
-
-%% SECOND FINCH PLOT
-
-dataFolder = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/ARChroma/Meetings/Meeting_April_23/';
-FinchData = readtable([dataFolder 'Finch_data_RedVsBlueErrorBars' '.csv']);
-lumRatio = 0:0.125:1;
-accommodation = [0.178 0.223 0.202 0.150 0.0890 0.031 -0.061 -0.209 -0.352];
-ubAcc = [0.204 0.251 0.231 0.178 0.124 0.064 -0.034 -0.177 -0.316];
-lbAcc = [0.146 0.191 0.163 0.122 0.052 -0.005 -0.089 -0.236 -0.387];
-figure;
-errorbar(lumRatio,accommodation,ubAcc-accommodation,accommodation-lbAcc,'ko','LineWidth',1,'MarkerSize',10,'MarkerFaceColor','k');
-hold on;
-plot(xlim,0.276.*[1 1],'r-','LineWidth',1);
-plot(xlim,-0.873.*[1 1],'b-','LineWidth',1);
-axis square;
-set(gca,'FontSize',15);
-xlabel('Blue/Red Luminance');
-ylabel('Accommodation');
-ylim([-1 0.3]);
-
--- ARCsignalQualityModeling/ARCwvInFocusConesMeanZsandbox.m
+++ ARCsignalQualityModeling/ARCwvInFocusConesMeanZsandbox.m
@@ -1,69 +1,67 @@
-function [wvInFocus, coneImgFilteredEg, coneImgOrigFilteredEg, wvInFocus2, wave, peakCorr, absorptionsReturn] = ARCwvInFocusConesMeanZsandbox(subjNum,stimNum,wLMS,waveInd,dataPath)
+function [wvInFocus, coneImgEg, coneImgOrigEg, wave, peakCorr, absorptionsReturn] = ARCwvInFocusConesMeanZsandbox(subjNum,stimNum,wLMS,waveInd,dataPath)
 
-% [wvInFocus, LminusMmechImg, coneImgOrigFiltered] = ARCwvInFocusConesMeanZsandbox(3,8,[0.375 -0.125 0]);
+% helper function for making Figure 6. 'Sandbox' version of the main model
+% function that saves out the cone images for wavelengths-in-focus of
+% interest. 
 
-if ispc
-   slash = '\';
-else
-   slash = '/';
-end
+% FOLDER WITH PRE-GENERATED CONE IMAGES
+foldernameCones = fullfile(dataPath,'data','coneImages');
+% FOLDER NAME WITH HELPER FILES
+foldernameHelpers = fullfile(dataPath,'data','helperFiles');
 
-wave = 380:4:780;
-nFocus = length(wave);
-foldernameCones = [dataPath 'data' slash 'coneImages' slash];
+wave = 380:4:780; % SAMPLED WAVELENGTHS IN MODEL
+nFocus = length(wave); % NUMBER OF WAVELENGTHS MODELED
 
-% USE THE SAME ORIGINAL (PRE-OPTICS) IMAGE EACH TIME--THIS ONE HAPPENS TO
-% LIVE IN THE FOLDER FOR SUBJECT 10, BUT IT REALLY DOESN'T MATTER SINCE ALL
-% SUBJECTS SAW THE SAME ON-SCREEN STIMULUS
-fnameConeRspNoLCA = ['subj10block3stimulus1' 'focusInd1noLCA'];
-absorptionsOrig = load([foldernameCones 'S10/' fnameConeRspNoLCA]);
+% LOAD THE ORIGINAL (PRE-OPTICS) IMAGE 
+% fnameConeRspNoLCA = 'subj10block3stimulus1focusInd1noLCA';
+fnameConeRspNoLCA = 'coneImagesNoOptics';
+absorptionsOrig = load(fullfile(foldernameHelpers,fnameConeRspNoLCA));
 absorptionsOrig = absorptionsOrig.absorptions;
 coneImgOrig = sum(absorptionsOrig,3);
 
 % LOAD SPATIAL FILTER
-load([dataPath 'data' slash 'modelParams' slash 'freqFilterARC.mat']);
+load(fullfile(foldernameHelpers,'freqFilterARC.mat'));
 
+% MASK FOR SIMULATING 'HOLE' IN S-CONE IMAGE. START OUT WITH SUPPORT.
 [SconeMaskSupportXX, SconeMaskSupportYY] = meshgrid(-90:91,-90:91);
+% START WITH A MASK WITH NO HOLE
 SconeMask = ones(size(SconeMaskSupportXX));
+% MAKE HOLE IN CENTER. THIS IS A HARD EDGE.
 SconeMask(sqrt(SconeMaskSupportXX.^2 + SconeMaskSupportYY.^2)<22.5) = 0;
+% NOW TO MAKE THE EDGE SOFT. WE ARE GOING TO CONVOLVE A BLUR KERNEL WITH
+% THE ENTIRE MASK. START WITH THE SUPPORT OVER THE KERNEL.
 [softEdgeSupportXX, softEdgeKernSupportYY] = meshgrid(linspace(-1,1,9));
+% USE A 2D GAUSSIAN TO MAKE THE KERNEL
 softEdgeKernCol = mvnpdf([softEdgeSupportXX(:) softEdgeKernSupportYY(:)],[0 0],[0.3^2 0; 0 0.3^2]); 
 softEdgeKern = reshape(softEdgeKernCol,size(softEdgeSupportXX))./sum(softEdgeKernCol(:));
+% CONVOLVE
 SconeMaskSoft = conv2(SconeMask,softEdgeKern);
+% REMOVE PADDING AT EDGES FROM THE RESULT OF THE CONVOLUTION
 SconeMask = SconeMaskSoft(5:186,5:186);
+% THE CONVOLUTION WILL CAUSE SOME VALUES TO BE BETWEEN 0 AND 1 AT THE
+% EDGES. WE WANT THESE VALUES TO BE EXACTLY 1.
 SconeMask(:,1:5) = 1;
 SconeMask(:,178:182) = 1;
 SconeMask(1:5,:) = 1;
 SconeMask(178:182,:) = 1;
 
-if wLMS(3) == 0
+if wLMS(3) == 0 % IF NOT BLUE-YELLOW MECHANISM, NO MASK
     SconeMask = ones(size(SconeMask));
 end
-
+% OTHERWISE, APPLY S-CONE MASK
 coneImgOrig = coneImgOrig.*SconeMask;
 
+% FILTER CONE IMAGE ACCORDING TO FREQUENCIES KNOWN TO DRIVE ACCOMMODATION
 coneImgOrigFFT = fftshift(fft2(coneImgOrig));
 coneImgOrigFilteredFFT = coneImgOrigFFT.*freqFilterARC;
 coneImgOrigFiltered = real(ifft2(ifftshift(coneImgOrigFilteredFFT)));
 
-% THE FOLLOWING LINES ARE JUST SANITY CHECKS
-% coneImgOrigFFT2 = fft2(fftshift(coneImgOrig));
-% freqFilterARCfft2 = fftshift(freqFilterARC);
-% coneImgOrigFilteredFFT2 = coneImgOrigFFT2.*freqFilterARCfft2;
-% coneImgOrigFiltered2 = real(ifftshift(ifft2(coneImgOrigFilteredFFT2)));
+peakCorr = []; % IMAGE QUALITY METRIC
+coneImgEg = []; % STORE EXAMPLE FILTERED CONE IMAGE
+coneImgOrigEg = []; % STORE ORIGINAL IMAGE FILTERED
+absorptionsReturn = []; % ABSORPTION VALUES TO SAVE OUT
 
-% % Put the image center in (1, 1) and take the transform.
-% imgFFT = fft2(fftshift(img));
-% % Multiply the transformed otf and the image.
-% % Then invert and put the image center in  the center of the matrix
-% filteredIMG = abs(ifftshift(ifft2(otf .* imgFFT)));
-
-peakCorr = [];
-coneImgFilteredEg = [];
-coneImgOrigFilteredEg = [];
-absorptionsReturn = [];
-
-if ~isempty(waveInd)
+if ~isempty(waveInd) % IF INDICES FOR WAVELENGTHS IN FOCUS OF INTEREST ARE SPECIFIED
     waveInd2examine = waveInd;
 else
     waveInd2examine = 1:nFocus;
@@ -71,36 +69,34 @@ end
 
 for i = waveInd2examine
     fnameConeRsp = ['subj' num2str(subjNum) 'stimulus' num2str(stimNum) 'focusInd' num2str(i)];
-    load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRsp]);
+    load(fullfile(foldernameCones,['S' num2str(subjNum)],fnameConeRsp));
+    % APPLY CONE WEIGHTS AND SUM
     absorptions(:,:,1) = SconeMask.*absorptions(:,:,1).*wLMS(1);
     absorptions(:,:,2) = SconeMask.*absorptions(:,:,2).*wLMS(2);
     absorptions(:,:,3) = SconeMask.*absorptions(:,:,3).*wLMS(3);
     coneImg = sum(absorptions,3);
-
+    % FILTER SIGNAL ACCORDING TO FREQUENCIES KNOWN TO DRIVE ACCOMMODATION
     coneImgFFT = fftshift(fft2(coneImg));
     coneImgFilteredFFT = coneImgFFT.*freqFilterARC;
     coneImgFiltered = real(ifft2(ifftshift(coneImgFilteredFFT)));
 
-    if i==waveInd
-        coneImgFilteredEg = coneImg;
-        coneImgOrigFilteredEg = coneImgOrig;
+    if i==waveInd % IF AT WAVELENGTH OF INTEREST
+        % SAVE OUT CONE IMAGES AS WELL AS ORIGINAL IMAGE AND ABSORPTIONS
+        coneImgEg = coneImg;
+        coneImgOrigEg = coneImgOrig;
         absorptionsReturn = absorptions;
     end
 
-    peakCorr(i) = max(max(normxcorr2(coneImgFiltered,coneImgOrigFiltered)));
+    % COMPUTE IMAGE QUALITY ACCORDING TO X-CORRELATION
+    peakCorr(i) = max(max(abs(normxcorr2(coneImgFiltered,coneImgOrigFiltered))));
 end
 
-if isempty(waveInd)
+if isempty(waveInd) % IF NOT LOOKING AT SPECIFIC WAVELENGTH IN FOCUS
     [~,indPeakPeak] = max(peakCorr);
     wvInFocus = wave(indPeakPeak);
-    
-    wave2 = wave(1:33);
-    peakCorr2 = peakCorr(1:33);
-    [~,indPeakPeak2] = max(peakCorr2);
-    wvInFocus2 = wave2(indPeakPeak2);
 else
+    % NO POINT IN COMPUTING BEST WAVELENGTH TO FOCUS
     wvInFocus = [];
-    wvInFocus2 = [];
 end
 
 end
